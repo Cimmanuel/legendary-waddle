28743a0fdcffa47cfbf95e15e68c4f29c88e9b4f
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-02-17 20:04:11 +0100

    feature: register custom User model in admin site

--- waddle/accounts/admin.py
+++ waddle/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "id", "email", "mobile", "is_active"]
+    list_filter = ["is_active"]
+    fieldsets = (
+        ("Personal info", {"fields": ("name", "email", "mobile", "password")}),
+        (
+            "Permissions", 
+            {
+                "fields": (
+                    "is_active",
+                    "is_admin",
+                    "is_staff",
+                    "is_superuser"
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.set_password(obj.password)
+        obj.save()
+        super().save_model(request, obj, form, change)
